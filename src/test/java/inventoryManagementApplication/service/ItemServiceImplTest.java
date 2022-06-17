1df5cd217d503eaf9bbd100e76df3864f2cc4b31
Andrew Blaeser <andblaeser@gmail.com>
2022-06-17 11:04:39 -0500

    add item service test

--- src/test/java/inventoryManagementApplication/service/ItemServiceImplTest.java
+++ src/test/java/inventoryManagementApplication/service/ItemServiceImplTest.java
@@ -1,5 +1,37 @@
 package inventoryManagementApplication.service;
 
-public class ItemServiceImplTest {
+import static org.mockito.Mockito.doReturn;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+
+import inventoryManagementApplication.model.Item;
+import inventoryManagementApplication.repository.ItemRepository;
+
+@SpringBootTest
+public class ItemServiceImplTest {
+	@Autowired
+	private ItemService service;
+
+	@MockBean
+	private ItemRepository repository;
+
+	@Test
+	void testGetItem() {
+		Item item = new Item();
+		item.setId((long) 1);
+		doReturn(item).when(repository).getById((long) 1);
+
+		try {
+			Item returnedItem = service.getItem((long) 1);
+			Assertions.assertTrue(returnedItem != null);
+			Assertions.assertSame(item, returnedItem);
+		} catch (NotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
