877b084e8aa419aa960cb4d91c9314985e64c88a
Andrew Blaeser <andblaeser@gmail.com>
2022-06-17 14:27:39 -0500

    add inventory and item repository tests

--- src/test/java/inventoryManagementApplication/repository/ItemRepositoryTest.java
+++ src/test/java/inventoryManagementApplication/repository/ItemRepositoryTest.java
@@ -1,5 +1,30 @@
 package inventoryManagementApplication.repository;
 
-public class ItemRepositoryTest {
+import javax.persistence.EntityManager;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class ItemRepositoryTest {
+	@Autowired
+	ItemRepository itemRepository;
+
+	@Autowired
+	EntityManager entityManager;
+
+	@Test
+	public void testFindAll() {
+
+	}
+
+	@Test
+	public void testGetById() {
+
+	}
 }
