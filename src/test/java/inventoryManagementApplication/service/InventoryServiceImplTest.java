2bf90e4ddd660d3f48cf95abf2f268b11e704db1
Andrew Blaeser <andblaeser@gmail.com>
2022-06-17 11:12:43 -0500

    add inventory service test

--- src/test/java/inventoryManagementApplication/service/InventoryServiceImplTest.java
+++ src/test/java/inventoryManagementApplication/service/InventoryServiceImplTest.java
@@ -1,5 +1,38 @@
 package inventoryManagementApplication.service;
 
+import static org.mockito.Mockito.doReturn;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException;
+
+import inventoryManagementApplication.model.Inventory;
+import inventoryManagementApplication.repository.InventoryRepository;
+
+@SpringBootTest
 public class InventoryServiceImplTest {
+	@Autowired
+	private InventoryService service;
+
+	@MockBean
+	private InventoryRepository repository;
+
+	@Test
+	void testGetInventory() {
+		Inventory inventory = new Inventory();
+		inventory.setId((long) 1);
+		doReturn(inventory).when(repository).getById((long) 1);
+
+		try {
+			Inventory returnedInventory = service.getInventory((long) 1);
+			Assertions.assertTrue(returnedInventory != null);
+			Assertions.assertSame(inventory, returnedInventory);
+		} catch (NotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
