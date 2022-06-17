c94ef68efcbc071fcffee771d489699e3f971e61
Andrew Blaeser <andblaeser@gmail.com>
2022-06-17 13:46:14 -0500

    add user repository test

--- src/test/java/inventoryManagementApplication/repository/UserRepositoryTest.java
+++ src/test/java/inventoryManagementApplication/repository/UserRepositoryTest.java
@@ -1,28 +1,37 @@
 package inventoryManagementApplication.repository;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import javax.persistence.EntityManager;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import inventoryManagementApplication.model.User;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
-	@Autowired
-	private TestEntityManager entityManager;
-
 	@Autowired
 	UserRepository userRepository;
 
-	@Test
-	public void findByEmailTest() {
-		User user1 = new User("John", "Doe", "johndoe@email.com", "password");
-		entityManager.persist(user1);
-		User user2 = new User("Demo", "User", "demo-user@email.com", "demo");
-		entityManager.persist(user2);
-		User foundUser = userRepository.findByEmail(user2.getEmail());
-		assertTrue(foundUser.equals(user2));
+	@Autowired
+	private EntityManager entityManager;
+
+	@ParameterizedTest
+	@ValueSource(strings = { "userone@email.com", "usertwo@email.com" })
+	public void findByEmailTest(String email) {
+		User user = new User();
+		user.setEmail(email);
+		entityManager.persist(user);
+		entityManager.flush();
+		entityManager.clear();
+		Assertions.assertTrue(userRepository.findByEmail(email) != null);
+		entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
+		entityManager.flush();
+		entityManager.clear();
 	}
 }
