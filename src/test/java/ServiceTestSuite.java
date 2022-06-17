2d820eea9edb38933594322c987c48776d56d017
Andrew Blaeser <andblaeser@gmail.com>
2022-06-17 11:19:28 -0500

    add service test suite

---
+++ src/test/java/ServiceTestSuite.java
@@ -0,0 +1,8 @@
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@SelectPackages("inventoryManagementApplication.service")
+@Suite
+public class ServiceTestSuite {
+
+}
