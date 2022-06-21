750831064dccc3ba6ef6304d80563661338bc992
Andrew Blaeser <andblaeser@gmail.com>
2022-06-21 12:51:53 -0500

    merged item repository test into inventory repository test class

--- src/test/java/inventoryManagementApplication/repository/InventoryRepositoryTest.java
+++ src/test/java/inventoryManagementApplication/repository/InventoryRepositoryTest.java
@@ -1,35 +1,59 @@
 package inventoryManagementApplication.repository;
 
-import javax.persistence.EntityManager;
+import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import inventoryManagementApplication.model.Inventory;
+import inventoryManagementApplication.model.Item;
+
 @DataJpaTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(MockitoExtension.class)
 public class InventoryRepositoryTest {
 	@Autowired
 	InventoryRepository inventoryRepository;
 
 	@Autowired
-	EntityManager entityManager;
+	ItemRepository itemRepository;
 
 	@Test
-	public void testFindAll() {
+	public void testInventoryFindAll() {
+		List<Inventory> allInventorys = inventoryRepository.findAll();
+		Assertions.assertTrue(allInventorys.size() >= 1);
+	}
 
+	@Test
+	public void testItemFindAll() {
+		List<Item> allItems = itemRepository.findAll();
+		Assertions.assertTrue(allItems.size() >= 1);
 	}
 
 	@Test
 	public void testGetById() {
-
+		Item item = new Item();
+		Inventory inventory = new Inventory();
+		inventory.setItem(item);
+		inventoryRepository.save(inventory);
+		Item returnedItem = itemRepository.getById(item.getId());
+		Inventory returnedInventory = inventoryRepository.getById(inventory.getId());
+		Assertions.assertNotNull(returnedInventory);
+		Assertions.assertNotNull(returnedItem);
+		itemRepository.delete(returnedItem);
+		inventoryRepository.delete(returnedInventory);
 	}
 
 	@Test
 	public void testFindByCountEquals() {
-
+		List<Inventory> zeroStock = inventoryRepository.findByCountEquals(0);
+		Assertions.assertTrue(zeroStock.size() >= 1);
 	}
 }
